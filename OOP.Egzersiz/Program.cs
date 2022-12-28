15e18247dd4b29a633877a8aa0aacc2329c2d7d1
khas <khas@KHASPC1036>
2022-12-28 15:06:21 +0300

    Add project files.

---
+++ OOP.Egzersiz/Program.cs
@@ -0,0 +1,15 @@
+﻿using OOP.Egzersiz;
+
+internal class Program
+{
+
+    // İnsan isimli bir sınıf içinde; boy,kilo,yaş tanımlayıp bir kurucu metot içinde bu alanlar için varsyılan değer atamaları yapacağız.Son olarak "Tanis" isimli metotla girilen bilgileri kullanıcıya açıklayıcı şekilde görüntüleyen bir program yazınız.
+
+    //Örnek 
+    private static void Main(string[] args)
+    {
+        clsInsan Insan = new clsInsan();
+        Insan.Tanis();
+        
+    }
+}
---
+++ OOP.Egzersiz/clsInsan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Egzersiz
+{
+    internal class clsInsan
+    {
+        public string ad { get; set; }
+        public string soyad { get; set; }
+
+        public int yas { get; set; }
+
+        public int boy { get; set; }
+
+        public int kilo { get; set; }
+
+
+  
+        public clsInsan()
+        {
+            ad = "Gamze";
+            soyad = "Kara";
+            yas = 25;
+            boy = 164;
+            kilo = 55;
+
+                        
+        }
+
+        public void Tanis()
+        {
+            Console.WriteLine("\n Adım : {0}  \n Soyadım {1} \n Yasım {2} \n Boyum {3} \n Kilom {4}",ad,soyad,yas,boy,kilo);
+           
+        }
+            
+    }
+}
